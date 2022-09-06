679acd43d922425a89955467e01a2590b909e18f
김강현 <101877146+kimgh06@users.noreply.github.com>
2022-09-06 10:40:14 +0900

    Update 보조.js

--- 보조.js
+++ 보조.js
@@ -101,4 +101,17 @@ function alienIs() {
     }
 }
 
+function toggleMenu() {
+    if (document.querySelector("#toToggle").classList.contains("hidden")) {
+        document.querySelector("#toToggle").classList.remove("hidden");
+        document.querySelector("aside").style.backgroundColor = "rgb(105, 191, 225)"; //색깔 변경
+        document.querySelector("#main").style.marginLeft = "17%"; //(메인 좌측 여백 설정)
+    }
+    else {
+        document.querySelector("#toToggle").classList.add("hidden");
+        document.querySelector("aside").style.backgroundColor = "white"; //색깔 변경
+        document.querySelector("#main").style.marginLeft = "0%"; //(메인 좌측 여백 설정)
+    }
+}
+
 submitForm.addEventListener("submit", sub);
